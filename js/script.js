a452ad88e920aaf5e4b7f07ddfb8a5d8e309ca48
Scarycrumb45 <scarycrumb45@gmail.com>
2021-02-10 11:43:33 -0700

    Remove ITO

--- js/script.js
+++ js/script.js
@@ -116,6 +116,8 @@ function processSheets(transitSheet, dataSheet) {
   locationObjectObject = deepExtend(dataSheetAirportsObject, transitAirportsObject)
   locationObjectObject = deepExtend(heliportsObject, locationObjectObject)
 
+  delete locationObjectObject["ITO"] //ITO doesn't exist
+
   //convert into array
   let locationKeys = Object.keys(locationObjectObject);
   locationKeys.forEach((locationKey) => {
