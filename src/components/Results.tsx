63a475cb82fd5d782088903fd2c3811f22b8d4d0
Robbie <robbiethewadley@gmail.com>
2022-09-10 21:44:16 -0600

    add spinner

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,5 @@
 import React, { useContext, useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
 import FindPath from "../routing/findPath";
 import { RoutingContext } from "./Routing";
 
@@ -9,6 +10,7 @@ export default function Results() {
   useEffect(() => {
     if (from && to) {
       const findPath = new FindPath(from, to);
+      setMessage("Finding path...");
 
       findPath.findPath().then((results) => {
         setMessage(results.join(" -> "));
@@ -25,6 +27,23 @@ export default function Results() {
     <div>
       HERE:
       <div>{message}</div>
+      <Spinner />
     </div>
   );
 }
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }         
+`;
+
+const Spinner = styled.div`
+  width: 200px;
+  height: 200px;
+  border: 10px solid #f3f;
+  animation: ${spin} 2s linear infinite;
+`;
