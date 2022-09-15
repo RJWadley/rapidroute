999e6470ddb7c580a0fbb4d91f2b0752b106aebe
Robbie <robbiethewadley@gmail.com>
2022-09-14 20:56:04 -0600

    Single route

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -8,6 +8,7 @@ import { ScrollSmoother } from "gsap/ScrollSmoother"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 
 import Providers from "components/Providers"
+import invertLightness from "utils/invertLightness"
 
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
 
@@ -18,3 +19,5 @@ export const wrapRootElement = ({ element }: { element: React.ReactNode }) => {
 export const wrapPageElement = ({ element }: { element: React.ReactNode }) => {
   return element
 }
+
+console.log(invertLightness("#00ffff"))
--- src/components/Segment/SingleRoute.tsx
+++ src/components/Segment/SingleRoute.tsx
@@ -1,10 +1,11 @@
 import React, { useMemo, useState } from "react"
 
-import styled from "styled-components"
+import styled, { css } from "styled-components"
 
 import { SegmentType } from "components/createSegments"
 import { getPath } from "data/getData"
 import { Provider } from "types"
+import invertLightness from "utils/invertLightness"
 
 interface SegmentProps {
   segment: SegmentType
@@ -25,40 +26,151 @@ export default function SingleRoute({ segment }: SegmentProps) {
     route?.type === "MRT"
       ? "https://www.minecartrapidtransit.net/wp-content/uploads/2015/01/logo.png"
       : provider?.logo
+  const themeColor = provider?.color?.light ?? "#eeeeee"
+  const rawFromGate = route?.locations[segment.from.uniqueId] ?? null
+  const fromGate = rawFromGate === "none" ? null : rawFromGate
+  const expandedFromGate = fromGate?.includes("T")
+    ? fromGate.split(" ").join(" Gate ").replace("T", "Terminal ")
+    : `Gate ${fromGate}`
+
+  const rawToGate = route?.locations[segment.to.uniqueId] ?? null
+  const toGate = rawToGate === "none" ? null : rawToGate
+  const expandedToGate = toGate?.includes("T")
+    ? toGate.split(" ").join(" Gate ").replace("T", "Terminal ")
+    : `Gate ${toGate}`
+
+  let routeNumberMessage = ""
+  switch (route?.type) {
+    case "flight":
+      routeNumberMessage = `Flight ${route?.number ?? ""}`
+      break
+    case "heli":
+      routeNumberMessage = `Helicopter Flight ${route?.number ?? ""}`
+      break
+    case "seaplane":
+      routeNumberMessage = `Seaplane Flight ${route?.number ?? ""}`
+      break
+    default:
+      routeNumberMessage = ""
+  }
 
   return (
-    <Wrapper themeColor={provider?.color?.light ?? "red"}>
+    <Wrapper
+      backgroundColor={themeColor}
+      textColor={invertLightness(themeColor)}
+    >
       <ProviderName>
-        {image && <Image src={image} alt={`${provider?.name} logo`} />}{" "}
-        <Name>{provider?.name}</Name>
+        {image && (
+          <Image
+            bigLogo={route?.type === "MRT"}
+            background={invertLightness(themeColor)}
+          >
+            <img src={image} alt={`${provider?.name} logo`} />
+          </Image>
+        )}
+        <div>
+          <Name>{provider?.name}</Name>
+          <Number>{routeNumberMessage}</Number>
+        </div>
+        <LongNames>
+          {segment.from.name} to <br />
+          {segment.to.name}
+        </LongNames>
       </ProviderName>
-      <div>
-        {segment.from.shortName} -&gt; {segment.to.shortName}
-      </div>
-      <div>
-        {segment.to.shortName} - {segment.to.name}
-      </div>
-      <div>{segment.routes[0]?.provider ?? "walk"}</div>
+
+      <Symbols>
+        <div>
+          {segment.from.shortName}
+          {fromGate && <GateNumber>{expandedFromGate}</GateNumber>}
+        </div>
+        <div>-&gt;</div>
+        <div>
+          {fromGate && <GateNumber>{expandedToGate}</GateNumber>}
+          {segment?.to?.shortName}
+        </div>
+      </Symbols>
     </Wrapper>
   )
 }
 
-const Wrapper = styled.div<{ themeColor: string }>`
+const Wrapper = styled.div<{
+  backgroundColor?: string
+  textColor?: string
+}>`
   font-family: Inter;
-  background-color: ${props => props.themeColor};
-`
-
-const ProviderName = styled.div`
-  display: flex;
-  align-items: center;
+  ${({ backgroundColor, textColor }) => css`
+    background-color: ${backgroundColor};
+    color: ${textColor};
+  `}
+  padding: 50px;
+  border-radius: 50px;
+  display: grid;
   gap: 20px;
 `
 
-const Image = styled.img`
-  max-height: 60px;
-  max-width: 60px;
+const ProviderName = styled.div`
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto 1fr auto;
+  gap: 20px;
+`
+
+const Image = styled.div<{ bigLogo: boolean; background: string }>`
+  border-radius: 10px;
+  background-color: ${({ background }) => background};
+  width: 80px;
+  height: 80px;
+
+  img {
+    ${props =>
+      props.bigLogo
+        ? css`
+            border-radius: 10px;
+          `
+        : css`
+            margin: 5px;
+            width: 70px;
+            height: 70px;
+          `}
+  }
 `
 
 const Name = styled.div`
-  font-weight: 100;
+  font-family: "Inter";
+  font-weight: 700;
+  font-size: 32px;
+`
+
+const Number = styled.div`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+`
+
+const LongNames = styled.div`
+  text-align: right;
+`
+
+const Symbols = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 48px;
+  font-weight: 700;
+
+  > div {
+    display: flex;
+    align-items: baseline;
+    gap: 10px;
+  }
+
+  > div:last-child {
+    text-align: right;
+  }
+`
+
+const GateNumber = styled.div`
+  font-size: 20px;
+  font-weight: normal;
 `
