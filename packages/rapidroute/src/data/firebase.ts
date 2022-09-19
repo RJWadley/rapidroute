7a51fa9d8a6dac8a10fad4b20cf2a3d76825536f
Robbie <robbiethewadley@gmail.com>
2022-09-19 01:02:20 -0600

    build fix

--- packages/rapidroute/src/data/firebase.ts
+++ packages/rapidroute/src/data/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app"
 import { getDatabase } from "firebase/database"
 import { getAnalytics } from "firebase/analytics"
+import { isBrowser } from "utils/functions"
 
 const firebaseConfig = {
   apiKey: "AIzaSyAk72DEr-1lB3XeRWIHKQ-yq_mTytWXxoo",
@@ -15,4 +16,4 @@ const firebaseConfig = {
 
 export const app = initializeApp(firebaseConfig)
 export const database = getDatabase(app)
-export const analytics = getAnalytics(app)
+export const analytics = isBrowser() && getAnalytics(app)
