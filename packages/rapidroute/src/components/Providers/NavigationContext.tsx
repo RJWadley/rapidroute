97e805f56df9a83e193dc363f001e19df00d9299
Robbie <robbiethewadley@gmail.com>
2022-11-05 19:18:33 -0600

    nav commentary

--- packages/rapidroute/src/components/Providers/NavigationContext.tsx
+++ packages/rapidroute/src/components/Providers/NavigationContext.tsx
@@ -3,11 +3,33 @@ import React, { createContext, ReactNode, useMemo, useState } from "react"
 import { SegmentType } from "components/createSegments"
 
 export const NavigationContext = createContext<{
+  /**
+   * The route the user would prefer to take, if possible
+   */
   preferredRoute: string[]
+  /**
+   * Update the route the user would prefer to take, if possible
+   */
   setPreferredRoute: (preferredRoute: string[]) => void
+  /**
+   * The route the navigation system is currently trying to take
+   */
   currentRoute: SegmentType[]
+  /**
+   * Update the route the navigation system is currently trying to take
+   */
   setCurrentRoute: (currentRoute: SegmentType[]) => void
+  /**
+   * The route the navigation system is using to give directions.
+   * This often differs from the current route, since it won't update until
+   * the user has reached a point of interest
+   * 
+   * (for example, while moving from stop to stop on an MRT line, updates are suppressed)
+   */
   spokenRoute: SegmentType[]
+  /**
+   * Update the route the navigation system is using to give directions
+   */
   setSpokenRoute: (spokenRoute: SegmentType[]) => void
 }>({
   preferredRoute: [],
--- packages/rapidroute/src/navigation/useNavigation.ts
+++ packages/rapidroute/src/navigation/useNavigation.ts
@@ -17,24 +17,35 @@ export default function useNavigation() {
     useContext(NavigationContext)
   const { allowedModes } = useContext(RoutingContext)
 
+  /**
+   * start voice navigation
+   */
+  useVoiceNavigation(spokenRoute)
+
+  /**
+   * Update the spoken route when needed
+   */
   useEffect(() => {
     const firstSpoken = spokenRoute[0]
-    const firstCurrent = currentRoute[0]
     const spokenProvider = firstSpoken?.routes[0]?.provider
-    const currentProvider = firstCurrent?.routes[0]?.provider
     const spokenNumber = stopToNumber(firstSpoken?.from.uniqueId)
-    const currentNumber = stopToNumber(firstCurrent?.from.uniqueId)
-    const toNumber = stopToNumber(firstSpoken?.to.uniqueId)
 
+    const firstCurrent = currentRoute[0]
+    const currentProvider = firstCurrent?.routes[0]?.provider
+    const currentNumber = stopToNumber(firstCurrent?.from.uniqueId)
+    const destinationNumber = stopToNumber(firstSpoken?.to.uniqueId)
+
+    // for MRT lines
     if (firstSpoken && firstCurrent) {
       // we are still going to the same stop
       if (firstSpoken.to.uniqueId === firstCurrent.to.uniqueId) {
         // and are still on the same line
         if (spokenProvider === currentProvider) {
-          // and we are in the middle of the route spoken
+          // and we are still within the bounds of the route we spoke
           if (
-            (spokenNumber < currentNumber && currentNumber < toNumber) ||
-            (spokenNumber > currentNumber && currentNumber > toNumber)
+            (spokenNumber < currentNumber &&
+              currentNumber < destinationNumber) ||
+            (spokenNumber > currentNumber && currentNumber > destinationNumber)
           ) {
             // the spoken route is still valid
             return undefined
@@ -43,53 +54,69 @@ export default function useNavigation() {
       }
     }
 
+    // if we reach this point, the spoken route is no longer valid and we need to update it
     return setSpokenRoute(currentRoute)
   }, [currentRoute, setSpokenRoute, spokenRoute])
 
+  /**
+   * Keep the current route up to date
+   */
   const destinationId = currentRoute[currentRoute.length - 1]?.to.uniqueId || ""
-
-  let pathfinder: FindPath | undefined
-
-  const updateRoute = () => {
-    const playersLocation = session.lastKnownLocation
-
-    if (playersLocation) {
-      const { x, z } = playersLocation
-      const coordId = `Coordinate: ${x}, ${z}`
-
-      pathfinder?.cancel()
-      pathfinder = new FindPath(coordId, destinationId, allowedModes)
-
-      pathfinder
-        .start()
-        .then(results => {
-          const result = results[0]
-          resultToSegments(result)
-            .then(segments => {
-              setCurrentRoute(segments)
-
-              // set point of interest
-              const poi = segments[0]?.to.location
-              session.pointOfInterest = poi
-            })
-            .catch(e => {
-              console.error("error converting result to segments during nav", e)
-              setCurrentRoute([])
-            })
-        })
-        .catch(e => {
-          console.error("Error while finding path during nav", e)
-          setCurrentRoute([])
-        })
-    }
-  }
-
-  useVoiceNavigation(spokenRoute)
-
   useEffect(() => {
+    /**
+     * Calculate and save a new route
+     */
+    const updateRoute = () => {
+      let pathfinder: FindPath | undefined
+
+      const playersLocation = session.lastKnownLocation
+      if (playersLocation) {
+        const { x, z } = playersLocation
+        const coordId = `Coordinate: ${x}, ${z}`
+
+        pathfinder?.cancel()
+        pathfinder = new FindPath(coordId, destinationId, allowedModes)
+
+        pathfinder
+          .start()
+          .then(results => {
+            const result = results[0]
+            resultToSegments(result)
+              .then(segments => {
+                /**
+                 * Update the current route
+                 */
+                setCurrentRoute(segments)
+
+                /**
+                 * set the point of interest
+                 */
+                const poi = segments[0]?.to.location
+                session.pointOfInterest = poi
+              })
+
+              /**
+               * If there's an issue, clear the route
+               */
+              .catch(e => {
+                console.error(
+                  "error converting result to segments during nav",
+                  e
+                )
+                setCurrentRoute([])
+              })
+          })
+          .catch(e => {
+            console.error("Error while finding path during nav", e)
+            setCurrentRoute([])
+          })
+      }
+    }
+
+    // update every 10 seconds
     const interval = setInterval(updateRoute, 10 * 1000)
     return () => {
       clearInterval(interval)
     }
-  })
+  }, [allowedModes, destinationId, setCurrentRoute])
 }
