96af8d601d7eaa0de4844956a6d6289bccc85fc0
Robbie <robbiethewadley@gmail.com>
2022-11-05 20:13:19 -0600

    keep walk routes in followed route

--- packages/rapidroute/src/navigation/useFollowedRoute.ts
+++ packages/rapidroute/src/navigation/useFollowedRoute.ts
@@ -1,9 +1,11 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
+
+import { useDeepCompareEffect } from "use-deep-compare"
 
 import { SegmentType } from "components/createSegments"
 
 /**
- * given the most up-to-date route from the player to the destination, 
+ * given the most up-to-date route from the player to the destination,
  * track and return the player's path from the initial navigation start to the player
  * @param route the current route the player is following
  * @returns the path from navigation start to the player now
@@ -12,7 +14,7 @@ export default function useFollowedRoute(route: SegmentType[]) {
   const [previousRoute, setPreviousRoute] = useState<SegmentType[] | null>(null)
   const [followedRoute, setFollowedRoute] = useState<SegmentType[]>([])
 
-  useEffect(() => {
+  useDeepCompareEffect(() => {
     // skip if we have no route or if first run
     if (!route.length) return
     if (previousRoute === null) {
@@ -20,20 +22,16 @@ export default function useFollowedRoute(route: SegmentType[]) {
       return
     }
 
-    // TODO this seems broken
-
     const firstSegment = route[0]
 
     const firstTo = previousRoute[0]?.to
     const secondTo = previousRoute[1]?.to
     const { from } = firstSegment
 
-    if (
-      firstTo.uniqueId === from.uniqueId ||
-      secondTo?.uniqueId === from.uniqueId
-    ) {
+    if (firstTo?.uniqueId === from.uniqueId)
       setFollowedRoute([...followedRoute, previousRoute[0]])
-    }
+    else if (secondTo?.uniqueId === from.uniqueId)
+      setFollowedRoute([...followedRoute, previousRoute[0], previousRoute[1]])
 
     setPreviousRoute(route)
   }, [followedRoute, previousRoute, route])
