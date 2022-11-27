403913a8010dbd0d06cab3bdbebfaf853d62c4ac
Robbie <robbiethewadley@gmail.com>
2022-11-27 03:31:39 -0700

    add offline support

--- packages/rapidroute/gatsby-config.ts
+++ packages/rapidroute/gatsby-config.ts
@@ -20,6 +20,7 @@ const config: GatsbyConfig = {
     "gatsby-plugin-image",
     "gatsby-plugin-sharp",
     "gatsby-transformer-sharp",
+    "gatsby-plugin-offline",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
