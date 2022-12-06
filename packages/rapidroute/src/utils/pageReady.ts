f4de49b36ce0904b9f1568f214b58b313fda9947
Robbie <robbiethewadley@gmail.com>
2022-12-05 20:35:36 -0700

    add loader utils

--- packages/rapidroute/src/utils/pageReady.ts
+++ packages/rapidroute/src/utils/pageReady.ts
@@ -5,18 +5,27 @@ import { sleep } from "./functions"
 let waitingForPage = true
 
 export async function pageReady(callback?: VoidFunction) {
+  // need to await in loop since pageReady is outside the react cycle
   // eslint-disable-next-line no-await-in-loop
   while (waitingForPage) await sleep(100)
 
   return callback?.()
 }
 
-export function useDocumentReady() {
+const functionsToRunOnUnmount: VoidFunction[] = []
+
+export function usePageReady() {
   useEffect(() => {
     waitingForPage = false
 
     return () => {
       waitingForPage = true
+      functionsToRunOnUnmount.forEach(fn => fn())
+      functionsToRunOnUnmount.length = 0
     }
   })
+}
+
+export function onUnmount(callback: VoidFunction) {
+  functionsToRunOnUnmount.push(callback)
 }
