b4411eb9e68e8a2e26a4e60ccdfcd28cb9eb239b
Robbie <robbiethewadley@gmail.com>
2023-03-10 22:22:13 -0700

    useInterval hook

---
+++ packages/rapidroute/src/utils/useInterval.ts
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from "react"
+
+type Callback = () => VoidFunction | void
+
+const useInterval = (callback: Callback, delay?: number | null) => {
+  const savedCallback = useRef<Callback>(() => {})
+
+  useEffect(() => {
+    savedCallback.current = callback
+  })
+
+  useEffect(() => {
+    if (delay !== null) {
+      let cleanup: VoidFunction | void
+
+      const interval = setInterval(() => {
+        if (cleanup) cleanup()
+        cleanup = savedCallback.current()
+      }, delay || 0)
+
+      return () => {
+        if (cleanup) cleanup()
+        clearInterval(interval)
+      }
+    }
+
+    return undefined
+  }, [delay])
+}
+
+export default useInterval
