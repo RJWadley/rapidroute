ebc2d0537d70a36fa90274f1066239054f2a7e93
Robbie <robbiethewadley@gmail.com>
2023-03-16 23:43:08 -0600

    cors

--- packages/rapidroute/src/components/MapSidebar/useWiki.ts
+++ packages/rapidroute/src/components/MapSidebar/useWiki.ts
@@ -7,7 +7,8 @@ import { ExtractResponse } from "types/wiki/ExtractQuery"
 import { WikiImage } from "types/wiki/ImageSearch"
 import { WikiResponse } from "types/wiki/PageSearch"
 
-export const WIKI_URL = "https://wiki.minecartrapidtransit.net/"
+export const WIKI_URL =
+  "http://cors.mrtrapidroute.com/?https://wiki.minecartrapidtransit.net/"
 
 /**
  * use the MediaWiki API to search for a page on the wiki and return the first paragraph.
