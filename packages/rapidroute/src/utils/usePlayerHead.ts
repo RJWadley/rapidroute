1816de6f884c0d0d14eb1377b305034fdc790544
Robbie <robbiethewadley@gmail.com>
2023-03-21 14:44:51 -0500

    fix player head loading

--- packages/rapidroute/src/utils/usePlayerHead.ts
+++ packages/rapidroute/src/utils/usePlayerHead.ts
@@ -6,17 +6,15 @@ const fallbackUUID = "ec561538-f3fd-461d-aff5-086b22154bce"
 
 export default function usePlayerHead(name: string | undefined) {
   const { data: mojangResponse, isLoading } = useQuery<MojangUUIDResponse>({
-    queryKey: [`https://api.mojang.com/users/profiles/minecraft/${name ?? ""}`],
+    queryKey: [
+      `https://cors.mrtrapidroute.com/?https://api.mojang.com/users/profiles/minecraft/${
+        name ?? ""
+      }`,
+    ],
     enabled: !!name,
   })
+
   const uuid = mojangResponse?.id ?? fallbackUUID
-
-  const { data: image } = useQuery({
-    queryKey: [`https://crafatar.com/avatars/${uuid}?overlay`],
-    enabled: !isLoading,
-    queryFn: ({ signal }) =>
-      fetch(`https://crafatar.com/avatars/${uuid}?overlay`, { signal }),
-  })
-
-  return image?.url
+  if (isLoading && name) return
+  return `https://crafatar.com/avatars/${uuid}?overlay`
 }
