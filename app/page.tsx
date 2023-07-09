1578e4293ac10acbf9caf797e71718415696be78
Robbie <robbiethewadley@gmail.com>
2023-07-08 23:02:55 -0600

    add layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,35 @@
-import Temp from "Temp"
+import { styled } from "@linaria/react"
+// TODO finish import
+// import Header from "components/Header"
+// import Layout from "components/Layout"
+// import Results from "components/Results"
+// import Selection from "components/Selection"
+// import Seo from "components/SEO"
 
-export default async function Index() {
-  return <Temp />
+export default function Home() {
+  return (
+    <Content>
+      <Title>
+        <Strong>MRT</Strong> Route Finder
+      </Title>
+      {/* <Selection />
+      <Results /> */}
+    </Content>
+  )
 }
+
+const Content = styled.div`
+  max-width: calc(100vw - 40px);
+  margin: 0 auto;
+`
+
+const Title = styled.div`
+  text-align: center;
+  font-size: var(--large);
+  margin-top: 110px;
+  margin-bottom: 50px;
+`
+
+const Strong = styled.span`
+  font-weight: 700;
+`
---
+++ app/utils/media.ts
@@ -0,0 +1,5 @@
+const media = {
+  mobile: "(max-width: 768px)",
+} as const
+
+export default media
