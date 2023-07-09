70d8d47ab8db11905ac59817d6bb156890490178
Robbie <robbiethewadley@gmail.com>
2023-07-08 23:42:46 -0600

    linting

--- app/updater/index.ts
+++ app/updater/index.ts
@@ -4,15 +4,12 @@ import importDynmapAirports from "./import/dynmapAirports"
 import importDynmapMRT from "./import/dynmapMRT"
 import { importTransitSheet } from "./import/transitSheet"
 
-const sleep = (ms: number) =>
-  new Promise((resolve) => {
-    setTimeout(resolve, ms)
-  })
-
 export const runImport = async () => {
   console.log("Importing...")
-  // await importDynmapAirports()
-  // await importDynmapMRT()
-  return importTransitSheet()
+
+  await importDynmapAirports()
+  await importDynmapMRT()
+  await importTransitSheet()
+
   console.log("Imported!")
 }
