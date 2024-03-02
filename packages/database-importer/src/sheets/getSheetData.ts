6c9ff09b8c1bab2e003687f5b6e3c690862f2b24
Robbie <robbiethewadley@gmail.com>
2024-03-02 13:42:18 -0700

    testing proxy

--- packages/database-importer/src/sheets/getSheetData.ts
+++ packages/database-importer/src/sheets/getSheetData.ts
@@ -612,7 +612,7 @@ const placeLocations: Record<
 function generateMrtFromMarkers(): Promise<boolean> {
   return new Promise(resolve => {
     fetch(
-      "https://dynmap.minecartrapidtransit.net/main/tiles/_markers_/marker_new.json"
+      "https://cors.mrtrapidroute.com?https://dynmap.minecartrapidtransit.net/main/tiles/_markers_/marker_new.json"
     )
       .then(response => {
         return response.json()
