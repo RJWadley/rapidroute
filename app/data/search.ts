ffe938d30ccd09f53edde4daaec91c4fae542fcd
Robbie <robbiethewadley@gmail.com>
2024-03-20 01:12:21 -0600

    trying out a different sort algorithm

--- app/data/search.ts
+++ app/data/search.ts
@@ -1,44 +1,23 @@
 import { Place } from "@prisma/client"
-import MiniSearch from "minisearch"
-import { useEffect, useMemo } from "react"
+import searcher from "fuzzysort"
+import { useDeferredValue, useMemo } from "react"
 
 export const useSearchResults = <T extends Partial<Place>>(
-  query: string | undefined | null,
-  initialPlaces: T[],
+  rawQuery: string | undefined | null,
+  initialPlaces: T[]
 ) => {
-  /**
-   * create our search indexes
-   */
-  const fuzzyMatcher = useMemo(() => {
-    return new MiniSearch<T>({
-      fields: ["id", "name", "IATA", "type", "worldName"],
-      searchOptions: {
-        prefix: true,
-        fuzzy: 0.2,
-      },
-    })
-  }, [])
+  const query = useDeferredValue(rawQuery)
 
-  /**
-   * add each item to the search index
-   */
-  useEffect(() => {
-    fuzzyMatcher.addAll(
-      initialPlaces.filter((item) => !fuzzyMatcher.has(item.id)),
-    )
-  }, [fuzzyMatcher, initialPlaces])
-
-  /**
-   * search for items based on the search term
-   */
   const results = useMemo(
-    () => (query ? fuzzyMatcher.search(query) : []),
-    [fuzzyMatcher, query],
+    () =>
+      query
+        ? searcher.go(query, initialPlaces, {
+            keys: ["name", "IATA", "type", "worldName"],
+            limit: 20,
+          })
+        : null,
+    [query, initialPlaces]
   )
-    .map((result) => initialPlaces.find((item) => item.id === result.id))
-    .filter(Boolean)
-    // only keep first 50
-    .slice(0, 50)
 
-  return query ? results : initialPlaces
+  return results ? results.map((r) => r.obj) : initialPlaces
 }
