5a82425dfd9e87b5a7c4fc47dd865b8ba42e6c4a
Robbie <robbiethewadley@gmail.com>
2024-06-17 16:13:12 -0600

    wip

--- work_in_progress/rapidroute/next.config.mjs
+++ work_in_progress/rapidroute/next.config.mjs
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { withPigment } from "@pigment-css/nextjs-plugin"
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {}
+
+export default withPigment(nextConfig, {
+    
+        theme: {
+          colors: {
+            primary: 'tomato',
+            secondary: 'cyan',
+          },
+          spacing: {
+            unit: 8,
+          },
+          typography: {
+            fontFamily: 'Inter, sans-serif',
+          },
+        },
+      
+})
