5deaaa6f01e6d979e45ba0cb12a73b007ac408f0
Robbie <robbiethewadley@gmail.com>
2024-07-28 14:56:15 -0600

    log race results with times

--- app/utils/racePromisesWithLog.ts
+++ app/utils/racePromisesWithLog.ts
@@ -5,14 +5,18 @@ export const racePromisesWithLog = async <T>(
 	promises: { name: string; promise: Promise<T> | null }[],
 ) => {
 	let hasWinner = false
+	const startTime = performance.now()
 
 	const loggablePromises = promises.map(({ name, promise }) =>
 		promise?.then(() => {
 			if (hasWinner) return
 			hasWinner = true
+			const elapsedTime = performance.now() - startTime
 
 			// TODO report this as analytics
-			console.log(`RACE RESULT: ${name} was faster!`)
+			console.log(
+				`RACE RESULT: ${name} was faster! Took ${elapsedTime.toFixed(1)}ms`,
+			)
 			return promise
 		}),
 	)
